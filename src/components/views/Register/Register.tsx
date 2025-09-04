cfe3ebbda5f34d6822e339b5c20295667cfb829d
edolaku <edoooq@gmail.com>
2025-09-04 15:01:28 +0700

    feat: setup register page (Sprint 1 - 18.UI Auth Register

---
+++ src/components/views/Register/Register.tsx
@@ -0,0 +1,46 @@
+import { Card, CardBody, Input } from "@heroui/react"
+import Image from "next/image"
+import Link from "next/link"
+
+const Register = () => {
+    return (
+        <div className="flex w-full flex-row items-center justify-center gap-20">
+            <div className="flex w-1/3 flex-col items-center justify-center gap-10">
+                <Image
+                    src="/images/general/logo.svg"
+                    alt="logo"
+                    width={180}
+                    height={180}
+                />
+
+                <Image
+                    src="/images/illustration/login.svg"
+                    alt="login"
+                    width={1024}
+                    height={1024}
+                />
+            </div>
+
+            <Card>
+                <CardBody className="p-8">
+                    <h2 className="text-xl font-bold text-danger-500">Create Account</h2>
+                    <p className="text-small mb-4">
+                        Have an account?&nbsp;
+                        <Link href="/login" className="font-semibold text-danger-400">Login here</Link>
+                    </p>
+                    <form className="flex w-80 flex-col">
+                        <Input
+                            type="text"
+                            label="Full Name"
+                            variant="bordered"
+                            autoComplete="off"
+                        />
+                    </form>
+
+                </CardBody>
+            </Card>
+        </div>
+    )
+}
+
+export default Register
